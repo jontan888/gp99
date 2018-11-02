3871c3ef3a0b01dc23c113fad22168bf7bb0fcbe
TARUC <TARUC@PC-84>
2018-11-02 10:56:03 +0800

    version 2 - attempt2

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,9 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("version 2");
         System.out.println("hello");
+        System.out.println("welcome");
     }
     
 }
